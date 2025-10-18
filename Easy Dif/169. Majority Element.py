eca2a5205877dece0477397b37213748bb4891b8
SlaPi <rusneacandrei@yahoo.com>
2025-10-18 18:40:45 +0300

    Blueprint

---
+++ Easy Dif/169. Majority Element.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        return sol.majorityElement(nums)
+
+
+nums= [3,2,3]
+sol = Solution()
+print(sol.majorityElement(nums))
