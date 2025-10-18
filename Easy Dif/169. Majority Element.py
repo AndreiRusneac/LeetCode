a27f52cbb46cd3ec7494b5448b937aeaae067687
SlaPi <rusneacandrei@yahoo.com>
2025-10-18 21:13:48 +0300

    Lol, holly functions

--- Easy Dif/169. Majority Element.py
+++ Easy Dif/169. Majority Element.py
@@ -1,8 +1,9 @@
 from typing import List
-
+import collections
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sol.majorityElement(nums)
+        counter = collections.Counter(nums)
+        return counter.most_common(1)[0][0]
 
 
 nums= [3,2,3]
